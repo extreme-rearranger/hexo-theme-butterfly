716caf8590eeadc2414b83c5ec1b384ec9a0edab
ex-rearranger <pamela7384@snu.ac.kr>
2025-01-23 02:07:33 +0900

    update to 92913a6193844516edb7e5f490ae559cd9dae9ac

--- scripts/filters/random_cover.js
+++ scripts/filters/random_cover.js
@@ -1,23 +1,27 @@
 'use strict'
 
 hexo.extend.filter.register('before_post_render', data => {  // extend post는 중복으로 인한 에러 발생
-  const recentCovers = []
-  const randomCoverFn = () => {
-    const { cover: { default_cover: defaultCover } } = hexo.theme.config
+  const previousIndexes = []
+  const getRandomCover = defaultCover => {
     if (!defaultCover) return false
     if (!Array.isArray(defaultCover)) return defaultCover
-    const defaultCoverLen = defaultCover.length
-    const limit = 3
 
-    let num
+    const coverCount = defaultCover.length
+    if (coverCount === 1) {
+      return defaultCover[0]
+    }
+    const maxPreviousIndexes = coverCount === 2 ? 1 : (coverCount === 3 ? 2 : 3)
+    let index
     do {
-      num = Math.floor(Math.random() * defaultCoverLen)
-    } while (recentCovers.includes(num))
+      index = Math.floor(Math.random() * coverCount)
+    } while (previousIndexes.includes(index) && previousIndexes.length < coverCount)
 
-    recentCovers.push(num)
-    if (recentCovers.length > limit) recentCovers.shift()
+    previousIndexes.push(index)
+    if (previousIndexes.length > maxPreviousIndexes) {
+      previousIndexes.shift()
+    }
 
-    return defaultCover[num]
+    return defaultCover[index]
   }
 
   const handleImg = data => {
@@ -26,15 +30,20 @@ hexo.extend.filter.register('before_post_render', data => {  // extend post는 �
 
     // Add path to top_img and cover if post_asset_folder is enabled
     if (hexo.config.post_asset_folder) {
-      if (topImg && topImg.indexOf('/') === -1 && imgTestReg.test(topImg)) data.top_img = `${data.path}${topImg}`
-      if (coverVal && coverVal.indexOf('/') === -1 && imgTestReg.test(coverVal)) data.cover = `${data.path}${coverVal}`
+      if (topImg && topImg.indexOf('/') === -1 && imgTestReg.test(topImg)) {
+        data.top_img = `${data.path}${topImg}`
+      }
+      if (coverVal && coverVal.indexOf('/') === -1 && imgTestReg.test(coverVal)) {
+        data.cover = `${data.path}${coverVal}`
+      }
     }
 
     if (coverVal === false) return data
 
     // If cover is not set, use random cover
     if (!coverVal) {
-      const randomCover = randomCoverFn()
+      const { cover: { default_cover: defaultCover } } = hexo.theme.config
+      const randomCover = getRandomCover(defaultCover)
       data.cover = randomCover
       coverVal = randomCover // update coverVal
     }
